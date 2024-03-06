42a388215364244a4deb88244e75d15b8a1cb5fe
Dope-Otaku <souvik.halder.btech2020@sitpune.edu.in>
2024-03-06 21:33:19 +0530

    added comments in ternary operator file

--- js/ternary operator/logic.js
+++ js/ternary operator/logic.js
@@ -1,7 +1,7 @@
 //ternary operators are shortcut for if else loop
 
-const price = 2000
-const discount = 10
+const price = 2000; //this is a price and can be changed
+const discount = 10; // this is a discount and can be changed
 
-const amount = price>1000 ? (price-(price*(discount/100))) : (price)  
-console.log(amount)
+const amount = price > 1000 ? price - price * (discount / 100) : price; //if price is greater than 1000 then discount will be applied else not
+console.log(amount);
