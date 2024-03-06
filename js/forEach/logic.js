9475f8a0b88bd721e675acceca11a6386e0d6034
Dope-Otaku <souvik.halder.btech2020@sitpune.edu.in>
2024-03-07 00:24:30 +0530

    added the definition and created a new function to convert only first letter into lowercase

--- js/forEach/logic.js
+++ js/forEach/logic.js
@@ -1,8 +1,8 @@
 // forEach is a method that is used to iterate over an array and perform a function on each element of the array. It is a higher order function that takes a callback function as an argument. The callback function is called for each element of the array. The callback function takes three arguments: the current element, the index of the current element, and the array itself. The forEach method does not return anything. It simply calls the callback function for each element of the array.(element, index, array)
 
-let numbers = [1, 2, 3, 4, 5];
+let numbers = ["apple", "orange", "cherry"];
 
-numbers.forEach(double);
+numbers.forEach(dis);
 numbers.forEach(display);
 
 function display(x) {
@@ -10,6 +10,13 @@ function display(x) {
 }
 function double(element, index, array) {
   array[index] = element;
+}
+
+//now only decapitalizze first letter of the fruits name and then run the code
+
+function dis(element, index, array) {
+  array[index] =
+    element.charAt(0).toLowerCase() + element.slice(1).toUpperCase();
 }
 
 //doubt: why does it prints only the value of double function and not the value of display function?
