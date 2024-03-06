273dfe44eee6b0488b8e3aaab49c3ac99ccf6c7b
Dope-Otaku <souvik.halder.btech2020@sitpune.edu.in>
2024-03-06 21:38:52 +0530

    added comments in js file of switches folder

--- js/switches/logic.js
+++ js/switches/logic.js
@@ -1,27 +1,27 @@
-let day = 232;
+let day = 232; //number of days can be changed here
 
-switch(day){
-    case 1:
-        console.log(`monday`)
-        break
-    case 2:
-        console.log('tuesday')
-        break
-    case 3:
-        console.log('wednesday')
-        break
-    case 4:
-        console.log('thursday')
-        break
-    case 5:
-        console.log('friday')
-        break
-    case 6:
-        console.log('saturday')
-        break
-    case 7:
-        console.log(`wohoo sunday!`)
-    default:
-        console.log(`${day} is not a day`)
-        break
+switch (day) {
+  case 1: //case: 1
+    console.log(`monday`);
+    break;
+  case 2: //case: 2
+    console.log("tuesday");
+    break;
+  case 3: //case: 3
+    console.log("wednesday");
+    break;
+  case 4: //case: 4
+    console.log("thursday");
+    break;
+  case 5: //case: 5
+    console.log("friday");
+    break;
+  case 6: //case: 6
+    console.log("saturday");
+    break;
+  case 7: //case: 7
+    console.log(`wohoo sunday!`);
+  default: //default case
+    console.log(`${day} is not a day`);
+    break;
 }
