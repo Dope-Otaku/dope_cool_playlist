9cb7f631ce45e968580fd279a949a73f91561c91
Dope-Otaku <souvik.halder.btech2020@sitpune.edu.in>
2024-03-06 21:50:56 +0530

    addded comments in forEach folder

--- js/forEach/logic.js
+++ js/forEach/logic.js
@@ -1,7 +1,9 @@
-let numbers = [1, 2, 3, 4, 5]
+// forEach is a method that is used to iterate over an array and perform a function on each element of the array. It is a higher order function that takes a callback function as an argument. The callback function is called for each element of the array. The callback function takes three arguments: the current element, the index of the current element, and the array itself. The forEach method does not return anything. It simply calls the callback function for each element of the array.
+
+let numbers = [1, 2, 3, 4, 5];
 
 numbers.forEach(display);
 
-function display(x){
-    console.log(x)
+function display(x) {
+  console.log(x);
 }
