cef6de39695a6994e342655bf90ff00e40f9fdbc
Dope-Otaku <souvik.halder.btech2020@sitpune.edu.in>
2024-03-07 01:01:33 +0530

    added the map folder with date spliter function will rememeber that split function is used seperate the array with a special operator icn is present in array itself and this also conatins a parameter that there is a limit

--- js/map/logic.js
+++ js/map/logic.js
@@ -8,7 +8,7 @@ const double = numbers.map((number) => {
 
 console.log(double); // [2,4,6,8,10]
 
-//method -
+//method - 2
 
 const students = ["ananya", "rahul", "viren", "suswik"];
 const upperCase = students.map(display);
@@ -18,3 +18,19 @@ console.log(upperCase);
 function display(elements, index, array) {
   return (array[index] = elements.charAt(0).toUpperCase() + elements.slice(1));
 }
+
+// date time slicing and positioning
+
+const dates = [
+  "2024.05.08",
+  "2023.05.09",
+  "2022.05.10",
+  "2021.05.11",
+  "2020.05.12",
+];
+const newDates = dates.map((date) => {
+  const parts = date.split(".");
+  return `${parts[2]} - ${parts[1]} - ${parts[0]}`;
+});
+
+console.log(newDates);
