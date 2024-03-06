e31032d231b8b831784195176b3abfd0726c36d6
Dope-Otaku <souvik.halder.btech2020@sitpune.edu.in>
2024-03-06 21:43:53 +0530

    added comments in js file of string methods folder

--- js/string methods/logic.js
+++ js/string methods/logic.js
@@ -1,15 +1,13 @@
 //string methods are basically string manipulation technique just like other programming language
-let string = "hello"
+let string = "hello";
 
-console.log(string.charAt(0))
-console.log(string.indexOf('o'))
-console.log(string.lastIndexOf('l'))
-console.log(string.length)
-console.log(string.trim()) //removes all trailing and white spaces between and outside the text
-console.log(string.toUpperCase())
+console.log(string.charAt(0)); //returns the character at the specified index
+console.log(string.indexOf("o")); //returns the index of the first occurrence of the specified value
+console.log(string.lastIndexOf("l")); //returns the index of the last occurrence of the specified value
+console.log(string.length); //returns the length of the string
+console.log(string.trim()); //removes all trailing and white spaces between and outside the text
+console.log(string.toUpperCase()); //converts the string to uppercase
 
+let number = 43434 - 34343 - 3434;
 
-
-let number = 43434-34343-3434
-
-console.log(number.replace)
+console.log(number.replace); //replaces the specified value with another value in a string
