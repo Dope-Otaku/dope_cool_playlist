ba541d795debc971c6dc2864f6899d2b19fce51a
Dope-Otaku <souvik.halder.btech2020@sitpune.edu.in>
2024-03-08 16:38:31 +0530

    this date filder has been added

---
+++ js/date/logic.js
@@ -0,0 +1,13 @@
+////date-objects contains date values
+
+const date = new Date();
+console.log(date);
+
+const year = date.getFullYear();
+console.log(year);
+
+const month = date.getMonth();
+console.log(month);
+
+const day = date.getDate();
+console.log(day);
